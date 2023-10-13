05ec31045c94b7631afc4bc10c5ece2c07f15884
Enis Abazi <itzzsineknej@protonmail.com>
2023-10-13 15:54:48 +0200

    uptaded
    uptaded

---
+++ script.js
@@ -0,0 +1,73 @@
+window.onload = function() {
+    const secondh1Element = document.querySelector('#secondCount');
+    let number = 0;
+
+    function secondNumber() {
+
+        if (number <= 5962) {
+            secondh1Element.textContent = number++;
+            number++;
+            setTimeout(secondNumber, .10)
+        }
+    }
+
+    secondNumber();
+}
+
+// secondtimeout
+
+
+window.onload = function() {
+    const secondh1Element = document.querySelector('#thirdCount');
+    let number = 0;
+
+    function secondNumber() {
+
+        if (number <= 2934) {
+            secondh1Element.textContent = number++;
+            number++;
+            setTimeout(secondNumber, .10)
+        }
+    }
+
+    secondNumber();
+}
+
+
+// 40392042402 timeout
+
+
+window.onload = function() {
+    const secondh1Element = document.querySelector('#fourthCount');
+    let number = 0;
+
+    function secondNumber() {
+
+        if (number <= 1439) {
+            secondh1Element.textContent = number++;
+            number++;
+            setTimeout(secondNumber, .10)
+        }
+    }
+
+    secondNumber();
+}
+
+// 5th function queryselectro
+
+
+window.onload = function() {
+    const secondh1Element = document.querySelector('#fifthCount');
+    let number = 0;
+
+    function secondNumber() {
+
+        if (number <= 933) {
+            secondh1Element.textContent = number++;
+            number++;
+            setTimeout(secondNumber, .10)
+        }
+    }
+
+    secondNumber();
+}
